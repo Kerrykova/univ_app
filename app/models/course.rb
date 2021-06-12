d9cc3ece9fa6f9d65c79a32baab5de723671a81f
Kerry Kovacik <kerrykovacik@gmail.com>
2021-06-12 10:37:16 -0700

    create student courses db
    Create student courses db via has_many associations. Added ability to post course enrollments using the course id and current user.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
 
     validates :name, presence: true, length: {minimum:5, maximum: 50}
-
+    has_many :student_courses
+    has_many :students, through: :student_courses
 end
