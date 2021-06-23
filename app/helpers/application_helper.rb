c729ad17b3c78bb93e5cfd26c1bfd59d71da0348
Kerry Kovacik <kerrykovacik@gmail.com>
2021-06-22 20:35:39 -0700

    fixed nav bar sign up button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,13 @@ module ApplicationHelper
         link_to("Sign Out", logout_path, method: :delete)
     else 
         link_to("Sign In", login_path)
-        link_to("Sign Up", new_student_path)
+    end
+    end
 
+    def signup_link
+        if (!logged_in?)
+        link_to("Sign Up", new_student_path)
     end
-    end
+    end 
 
 end
